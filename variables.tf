70f4f5e7aac7ddaf233c60fd4cfaab33da13f306
RekhuGopal <vrchinnarathod@gmail.com>
2023-06-09 11:05:30 +0530

    Vareiables

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "access_key" {
+    type = string 
+    sensitive = true
+  
+}
+
+variable "secret_key" {
+    type = string 
+    sensitive = true
+  
+}
+
+variable "token" {
+    type = string 
+    sensitive = true
+  
+}

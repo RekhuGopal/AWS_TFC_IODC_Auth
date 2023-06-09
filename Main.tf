7d9e80a39796fe01b99dd8480ec69103658609d3
RekhuGopal <vrchinnarathod@gmail.com>
2023-06-09 09:06:47 +0530

    Chanegd

--- Main.tf
+++ Main.tf
@@ -1,20 +1,3 @@
-## backend data for terraform
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-
-  cloud {
-  organization = "CloudQuickLabs"
-
-    workspaces {
-      name = "TFC-AWS-OIDC"
-    }
-  }
-}
-
 provider "aws" {
   region = "us-east-1"
   
---
+++ terraform.tf
@@ -0,0 +1,16 @@
+## backend data for terraform
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+
+backend "remote" {
+  organization = "CloudQuickLabs"
+
+    workspaces {
+      name = "TFC-AWS-OIDC"
+    }
+  }
+}

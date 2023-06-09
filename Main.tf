7e83dd3beb62e3615466b94efe03a5076513262b
RekhuGopal <vrchinnarathod@gmail.com>
2023-06-09 08:51:34 +0530

    Updated

--- Main.tf
+++ Main.tf
@@ -17,6 +17,11 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  assume_role_with_web_identity {
+  role_arn                = "arn:aws:iam::357171621133:role/TFC-AWS-OIDC"
+  session_name            = "SESSION_NAME"
+  web_identity_token_file = "/Users/tf_user/secrets/web-identity-token"
+}
 }
 
 module "BackupIAM" {
